136bf2b384fa5daed693a20dfdc9a11a8ec50df9
Your Name <you@example.com>
2019-09-19 19:34:14 -0700

    finished third route

--- routes/index.js
+++ routes/index.js
@@ -2,13 +2,21 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/', function (req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
 /* GET hello listing. */
-router.get('/hello', function(req, res, next) {
+router.get('/hello', function (req, res, next) {
   res.render('hello', { title: 'Hello World' });
+});
+
+/* GET time listing. */
+router.get('/time', function (req, res, next) {
+  res.render('time', {
+    title: 'CurrentTime',
+    date: new Date().toString()
+  });
 });
 
 module.exports = router;
