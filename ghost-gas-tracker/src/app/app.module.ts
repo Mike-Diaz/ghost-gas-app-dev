558d76471ed023b90e5888be9e90c4a138e9be36
Mike <tk7294@gmail.com>
2019-10-18 02:12:15 -0700

    feat: create user dash new branch
    Added a dashboard page which currently is the main app.component.ts and a header component.

--- ghost-gas-tracker/src/app/app.module.ts
+++ ghost-gas-tracker/src/app/app.module.ts
@@ -3,24 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MatSliderModule } from '@angular/material/slider';
-import { MatToolbarModule } from '@angular/material/toolbar';
-import { HeaderComponent } from './layout/header/header.component';
-import { VehicleEditComponent } from './vehicle-edit/vehicle-edit.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    HeaderComponent,
-    VehicleEditComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule,
-    BrowserAnimationsModule,
-    MatSliderModule,
-    MatToolbarModule
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
