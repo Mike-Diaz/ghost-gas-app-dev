69801fa60633d65fae5ac196771fa0ca9ca8c3de
austins <austins@users.noreply.github.com>
2019-10-22 15:15:23 -0700

    Import MatIconModule

--- ghost-gas-tracker/src/app/app.module.ts
+++ ghost-gas-tracker/src/app/app.module.ts
@@ -6,6 +6,7 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatSliderModule } from '@angular/material/slider';
 import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
 import { HeaderComponent } from './layout/header/header.component';
 import { VehicleEditComponent } from './vehicle-edit/vehicle-edit.component';
 
@@ -21,7 +22,8 @@ import { VehicleEditComponent } from './vehicle-edit/vehicle-edit.component';
     BrowserAnimationsModule,
     MatSliderModule,
     MatToolbarModule,
-    MatInputModule
+    MatInputModule,
+    MatIconModule
   ],
   providers: [],
   bootstrap: [AppComponent]
