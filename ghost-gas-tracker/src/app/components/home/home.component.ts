a85cd96388b1c870afe31274d242128593cd560f
Jordan R <you@example.com>
2019-11-05 18:47:05 -0800

    added homepage

--- ghost-gas-tracker/src/app/components/home/home.component.ts
+++ ghost-gas-tracker/src/app/components/home/home.component.ts
@@ -7,9 +7,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
+
+  
   constructor() { }
 
   ngOnInit() {
+    function onSignIn(googleUser) {
+      var profile = googleUser.getBasicProfile();
+      console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
+      console.log('Name: ' + profile.getName());
+      console.log('Image URL: ' + profile.getImageUrl());
+      console.log('Email: ' + profile.getEmail()); // This is null if the 'email' scope is not present.
+    }
+    
   }
-
+ 
 }
