b41bc752c06e7baab239f088ad9475bcd909b363
Jordan R <you@example.com>
2019-11-05 18:37:01 -0800

    fixed localhost 3k error

--- NodeMongoServer/app.js
+++ NodeMongoServer/app.js
@@ -15,7 +15,7 @@ require("./config/db");
 
 const app = express();
 
-const port = process.env.PORT || 3000;  // setting the port number for this server
+const port = 3000;  // setting the port number for this server
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
