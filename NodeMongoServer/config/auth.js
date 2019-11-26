9307a2619c76241a2853215365b1f94838f463de
austins <austins@users.noreply.github.com>
2019-11-26 10:50:44 -0800

    Fix auth.js passport parameters and save

--- NodeMongoServer/config/auth.js
+++ NodeMongoServer/config/auth.js
@@ -1,5 +1,5 @@
 const GoogleStrategy = require('passport-google-oauth20');
-const user = require('../models/User');
+const User = require('../models/User');
 module.exports = (passport) => {
 
     passport.serializeUser((user, done) => {
@@ -13,20 +13,20 @@ module.exports = (passport) => {
             clientSecret: 'DvzTotxtCsMFo8NcCXE_7r50',
             callbackURL: 'http://localhost:3000/auth/google/callback',
             passReqToCallback: true
-        }, 
-        (token, refreshToken, profile, done) => {
-            console.log('Getting User Information')
-            console.log(profile);
-            new user({
-                userID:profile.id,
-                name:profile.givenName + ' ' + profile.familyName,
+        },
+        (request, accessToken, refreshToken, profile, done) => {
+            console.log('Getting User Information');
+            new User({
+                organizationId: '1', // example organizationId since it's required in the schema?
+                userId:profile.id,
+                name:profile.name.givenName + ' ' + profile.name.familyName,
                 email:profile.emails[0].value,
                 picture:profile.photos[0].value
-            }).save().then((newUser) => {
+            }).save((err, newUser) => {
                 console.log('new user created: ' + newUser );
-            })
+            });
             return done(null, {
-                token: token
+                token: accessToken
             });
         }));
 };
