949b7501eafb00c35520664901d90b1492583a0d
Mike <tk7294@gmail.com>
2019-12-03 08:42:38 -0800

    implemented google login

--- ghost-gas-tracker/src/app/components/home/home.component.ts
+++ ghost-gas-tracker/src/app/components/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+import { AuthService, GoogleLoginProvider } from 'angular4-social-login';
 
 @Component({
   selector: 'app-home',
@@ -6,8 +7,32 @@ import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
-  
-  constructor() { }
+  title = 'Sign in';
+  user: any;
+  loggedIn: boolean = false;
+
+  constructor(private _socioAuthServ: AuthService) { }
+
+  singIn(platform : string): void {
+    platform = GoogleLoginProvider.PROVIDER_ID;
+    this._socioAuthServ.signIn(platform).then(
+      (response) => {
+        console.log(platform + " logged in user data is= " , response);
+        this.user = response;
+        this.loggedIn = true;
+        this.loggedInEvent.emit(this.loggedIn);
+      }
+    );
+    
+  }
+
+  signOut(): void {
+    this._socioAuthServ.signOut();
+    this.user = null;
+    this.loggedIn = false;
+    this.loggedInEvent.emit(this.loggedIn);
+    console.log('User signed out.');
+  }
 
   ngOnInit() {
     function onSignIn(googleUser) {
@@ -18,8 +43,6 @@ export class HomeComponent implements OnInit {
       console.log('Email: ' + profile.getEmail()); // This is null if the 'email' scope is not present.
     }
   }
-
-  loggedIn: boolean = true;
 
   @Output() loggedInEvent = new EventEmitter<boolean>();
 
